cd43673d6d48ec28abf30091615e3994201c01a4
Kot <umaduy@yandex.ru>
2024-01-23 06:16:57 +0300

    Элементы большие предидущего эл-та в массиве.

--- Task_23v.py
+++ Task_23v.py
@@ -5,7 +5,4 @@
 list_1 = [0, -1, 5, 2, 3, 8, 7]
 count = 0
 
-for i in range(1, len(list_1)):
-    if list_1[i] > list_1[i - 1]:
-        count += 1
-print(count)
+print(sum([1 for i in range(1, len(list_1)) if list_1[i] > list_1[i - 1]]))
